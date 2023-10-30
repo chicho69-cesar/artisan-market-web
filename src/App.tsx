d92ff28da57c888f6d78092599373a94683fbd7f
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-10-30 10:29:19 -0600

    Configure tailwind css for the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from '/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState(null)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('http://127.0.0.1:8000/api/products/get-products')
+      const data = await response.json()
+
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
+
+  useEffect(() => {
+    console.log(products)
+  }, [products])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-3xl font-bold text-center'>
+        Hello world!
+      </h1>
     </>
   )
 }
