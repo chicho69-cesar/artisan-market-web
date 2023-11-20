e32985ff7ea99ffcd50e03710230e96d33a8aeb4
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-11-19 20:45:54 -0600

    Remove CSS and React Strict Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from '@/App'
@@ -8,7 +8,7 @@ import App from '@/App'
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 )
