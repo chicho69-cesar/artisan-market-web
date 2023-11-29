0b8c191fda95d9fb15802b9adde8758f282d0b72
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-11-29 08:08:51 -0600

    Add profile information component

---
+++ src/modules/accounts/components/index.ts
@@ -0,0 +1,3 @@
+export { default as HeaderProfileActions } from './HeaderProfileActions'
+export { default as ProfileInformation } from './ProfileInformation'
+export { default as SocialNetwork } from './SocialNetwork'
