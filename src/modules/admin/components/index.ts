c0ccd817ead5ce1a562be6f11a3053ecfb17c637
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-12-03 21:47:27 -0600

    Add components folders barel files

---
+++ src/modules/admin/components/index.ts
@@ -0,0 +1,4 @@
+export { default as AdminProduct } from './AdminProduct'
+export { default as DashboardCard } from './DashboardCard'
+export { default as ImagePicker } from './ImagePicker'
+export { default as ProductImages } from './ProductImages'
---
+++ src/modules/cart/components/index.ts
@@ -0,0 +1,2 @@
+export { default as CartResume } from './CartResume'
+export { default as ProductCart } from './ProductCart'
