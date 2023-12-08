db5d0827c55d3de7b4a4dc3f0a12d905561db6ae
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-12-07 21:13:20 -0600

    Add recover password screen

--- src/modules/auth/screens/RecoverPasswordScreen.tsx
+++ src/modules/auth/screens/RecoverPasswordScreen.tsx
@@ -1,5 +1,48 @@
+import { useEffect } from 'react'
+
+import { AppButton, AppContainer, AppHeader, AppInput } from '@/modules/shared/components'
+import { useNavigate } from '@/modules/shared/hooks'
+import { useTheme } from '@/modules/shared/store'
+
 export default function RecoverPasswordScreen() {
+  const theme = useTheme((state) => state)
+  const { navigate } = useNavigate()
+
+  useEffect(() => {
+    theme.changeMainColor()
+  }, [])
+
   return (
-    <div>RecoverPasswordScreen</div>
+    <AppContainer>
+      <AppHeader
+        title='Recupera tu contraseña'
+        description='Te enviaremos una nueva contraseña de acceso a tu correo.'
+      />
+
+      <AppInput
+        isInvalid={false}
+        label='Email'
+        type='text'
+        onChangeText={() => {}}
+        placeholder='Email'
+        errorMessage='This is not a valid email'
+      />
+
+      <div className='mt-4' />
+
+      <AppButton
+        text='Enviar contraseña de acceso'
+        onPress={() => {}}
+      />
+
+      <section>
+        <p className='mt-6 font-medium text-center text-gray'>
+          ¿Aún no tienes cuenta?{' '}
+          <span onClick={() => navigate('/sign-up')} className={`font-bold cursor-pointer text-${theme.mainColor}`}>
+            Regístrate
+          </span>
+        </p>
+      </section>
+    </AppContainer>
   )
 }
