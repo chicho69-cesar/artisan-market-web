4739fc2bd8cd5af12f1a290c014f020641a9489f
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-12-07 21:13:35 -0600

    Add home screen

--- src/modules/products/screens/HomeScreen.tsx
+++ src/modules/products/screens/HomeScreen.tsx
@@ -1,5 +1,27 @@
+import { useEffect } from 'react'
+
+import { AppContainer, AppHeader } from '@/modules/shared/components'
+import { useTheme } from '@/modules/shared/store'
+import { Products } from '../components'
+import { useProducts } from '../hooks'
+
 export default function HomeScreen() {
+  const theme = useTheme((state) => state)
+  const { products, refetchProducts } = useProducts()
+
+  useEffect(() => {
+    theme.changeMainColor()
+    refetchProducts()
+  }, [])
+
   return (
-    <div>HomeScreen</div>
+    <AppContainer>
+      <AppHeader
+        title='ArtisanMarket'
+        description='Todos los productos'
+      />
+
+      <Products products={products} />
+    </AppContainer>
   )
 }
